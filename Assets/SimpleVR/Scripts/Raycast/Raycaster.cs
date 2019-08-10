b891fbdf0491b062423ba4e72b13f8723afe27d1
iln <romanhvorostov87@gmail.com>
2019-08-10 19:47:02 +0700

    Select/Deselect

---
+++ Assets/SimpleVR/Scripts/Raycast/Raycaster.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+public class Raycaster : MonoBehaviour
+{
+    [SerializeField]
+    private float maxDistanceRay = 10f;
+    private RaycastHit raycast;
+    private Vector3 dir = default;
+    private void FixedUpdate()
+    {
+        dir = transform.TransformDirection(Vector3.forward);
+        if (Physics.Raycast(transform.position, dir, out raycast, maxDistanceRay))
+        {
+            Debug.DrawRay(transform.position, dir, Color.red);
+            Debug.Log(raycast.collider.name);
+        }
+
+    }
+}
