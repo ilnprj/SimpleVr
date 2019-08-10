88cad1a7980dbbf9c3da1b4861fb41d1a1d94fb0
iln <romanhvorostov87@gmail.com>
2019-08-10 20:06:00 +0700

    Working on SOLID

--- Assets/SimpleVR/Scripts/Raycast/Raycaster.cs
+++ Assets/SimpleVR/Scripts/Raycast/Raycaster.cs
@@ -2,18 +2,55 @@
 
 public class Raycaster : MonoBehaviour
 {
+    public System.Action<bool> SelectStatusHandler = delegate { };
+    private bool hit;
+    public bool Hit
+    {
+        get
+        {
+            return hit;
+        }
+
+        set
+        {
+            if (hit != value)
+            {
+                SelectStatusHandler?.Invoke(value);
+                if (_targetManager!=null)
+                _targetManager.HandlingTarget(raycast.collider.gameObject);
+            }
+            hit = value;
+        }
+    }
+
     [SerializeField]
     private float maxDistanceRay = 10f;
     private RaycastHit raycast;
     private Vector3 dir = default;
+    private ITargetManager _targetManager;
+    private void Awake()
+    {
+        _targetManager = GetComponent<ITargetManager>();
+
+        if (_targetManager == null)
+        {
+            Debug.LogWarning("Target Manager is not found! Please check the it.");
+        }
+    }
+
     private void FixedUpdate()
     {
         dir = transform.TransformDirection(Vector3.forward);
         if (Physics.Raycast(transform.position, dir, out raycast, maxDistanceRay))
         {
+            Hit = true;
+#if UNITY_EDITOR
             Debug.DrawRay(transform.position, dir, Color.red);
-            Debug.Log(raycast.collider.name);
+#endif
         }
-
+        else
+        {
+            Hit = false;
+        }
     }
 }
