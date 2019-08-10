b81a5a660164850d032053456a05c77295b64981
iln <romanhvorostov87@gmail.com>
2019-08-10 19:11:44 +0700

    Add mouse movement for editor's tests.

---
+++ Assets/SimpleVR/Scripts/DebugRay.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+public class DebugRay : MonoBehaviour
+{
+#if UNITY_EDITOR
+    void Update()
+    {
+        Vector3 forward = transform.TransformDirection(Vector3.forward) * 5;
+        Debug.DrawRay(transform.position, forward, Color.green);
+    }
+#endif
+}
